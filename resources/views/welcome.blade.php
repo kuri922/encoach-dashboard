6c7d8297a04956baad80b0da4fbe45cb90e86a54
栗山雅史 <kuri.kuri922@gmail.com>
2021-12-07 16:36:27 +0900

    heroku

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -100,13 +100,13 @@
 
 
 <!DOCTYPE html>
-    <html lang="en">
+    <html lang="ja">
     <head>
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-        <link href = "{{ asset('css/encoach.css')}}" rel = "stylesheet">
+        <link href = "{{ asset('css/encoach.css') }}" rel = "stylesheet">
     </head>
     <body>
         <header class="toppage">
